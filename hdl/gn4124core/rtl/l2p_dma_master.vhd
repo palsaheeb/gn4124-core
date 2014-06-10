90775b394f55d16c66b5742d309acb8dc1c3f50a
Timon Heim <timon.heim@cern.ch>
2014-06-10 11:15:31 +0200

    Fixing reference to the right library.

--- hdl/gn4124core/rtl/l2p_dma_master.vhd
+++ hdl/gn4124core/rtl/l2p_dma_master.vhd
@@ -44,7 +44,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 use work.gn4124_core_pkg.all;
-use work.common_pkg.all;
+use work.genram_pkg.all;
 
 
 entity l2p_dma_master is
