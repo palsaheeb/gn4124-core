d434b7b639dcc62289c911eb8fc97966d8497fac
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2016-03-18 02:05:48 +0100

    Upgrade hdlmake support in the SPEC syn demo

--- hdl/spec/syn/Manifest.py
+++ hdl/spec/syn/Manifest.py
@@ -6,6 +6,7 @@ syn_grade = "-3"
 syn_package = "fgg484"
 syn_top = "spec_gn4124_test"
 syn_project = "spec_gn4124_test.xise"
+syn_tool = "ise"
 
 files = ["../spec_gn4124_test.ucf",
 	 "../ip_cores/l2p_fifo.ngc"]
@@ -13,7 +14,7 @@ files = ["../spec_gn4124_test.ucf",
 modules = { "local" : ["../rtl",
                        "../../common/rtl",
                        "../../gn4124core/rtl"],
-            "git" : "git://ohwr.org/hdl-core-lib/general-cores.git::master"}
+            "git" : "git://ohwr.org/hdl-core-lib/general-cores.git::proposed_master"}
 
 fetchto = "../ip_cores"
 
